79b76a6f65418f42a93631cfebf2d48659e52ec0
Now <mmkr4934@naver.com>
2022-03-03 15:00:32 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          깃허브 시작하기
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
